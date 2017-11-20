4e58e20a5e3717269637e1d207e7ce57dc4a2cc7
Swarbhanu Chatterjee <swarbhanu77@gmail.com>
2017-11-20 11:56:50 -0500

    Added method to dump excel with preprocessing as required

--- helpers.py
+++ helpers.py
@@ -3,19 +3,56 @@ import pandas as pd
 import re
 
 '''
+Dump columns from excel w/o processing in a csv file
+'''
+def dump_queries_from_excel(inputfile = '', outputfile = '', sheetnames = None, columns = None,
+                            append = False, process_queries=False, punctuations_to_remove = None):
+    mode = 'a' if append else 'w'
+    with open(outputfile, mode) as out:
+        if inputfile.endswith(".xlsx") or inputfile.endswith(".xls"):
+            with pd.ExcelFile(inputfile) as xls:
+                if not sheetnames:
+                    sheetnames = xls.sheet_names
+                for s in sheetnames:
+                    df = pd.read_excel(xls, sheetname=s, na_values=['NA'])
+                    outcols = [] # all inputs into a list
+                    for col in columns:
+                        outcol = []
+                        for l in df[col]:
+                            if isinstance(l, unicode) or isinstance(l, str):
+                                if process_queries:
+                                    l = convert_dates_to_dummy_day(l)
+                                    l = remove_punctuation(sentence=l, punctuations=punctuations_to_remove)
+                                if punctuations_to_remove:
+                                    l = remove_punctuation(sentence=l, punctuations=punctuations_to_remove)
+                                outcol.append(l)
+                        outcols.append(outcol)
+                    if len(outcols)==0:
+                        print "No columns got extracted"
+                        return
+                    for i in xrange(len(outcols[0])):
+                        for j in xrange(len(outcols)):
+                            line = [l[i] for l in outcols]
+                            line = ', '.join(line)
+                            for c in line:
+                                try:
+                                    out.write(c)
+                                except UnicodeEncodeError as e:
+                                    print "1.a Warning.", e.message, "Bad unicode character here: ", c
+                            out.write(u'\n')
+
+'''
 Read from excel file of particular format and generate an intermediate file with all queries
 
 Excel file format: Column 'Query' has all queries with definite intent. Other cols 'Query.1', 'Query.2', etc may or may not have definite intent
 '''
-def dump_queries(inputfile = '', outquery = '', sheetnames = None, columns_with_intent = None, columns_without_intent = None, intent = '', append = False, punctuations_to_remove = None):
-    if append:
-        mode = 'a'
-    else:
-        mode = 'w'
-
-    with open(outquery, mode) as out:
+def process_queries_from_excel(inputfile = '', outputfile = '', sheetnames = None, columns_with_intent = None,
+                 columns_without_intent = None, intent = '', append = False,
+                 process_queries=False,
+                 punctuations_to_remove = None):
+    mode = 'a' if append else 'w'
+    with open(outputfile, mode) as out:
         if inputfile.endswith(".xlsx") or inputfile.endswith(".xls"):
-            df = None
             with pd.ExcelFile(inputfile) as xls:
                 for s in sheetnames:
                     df = pd.read_excel(xls, sheetname=s, na_values=['NA'])
@@ -23,8 +60,9 @@ def dump_queries(inputfile = '', outquery = '', sheetnames = None, columns_with_
                         for l in df[col]:
                             if isinstance(l,unicode) or isinstance(l, str):
                                 try:
-                                    l = convert_dates_to_dummy_day(l)
-                                    l = remove_punctuation(sentence=l, punctuations=punctuations_to_remove)
+                                    if process_queries:
+                                        l = convert_dates_to_dummy_day(l)
+                                        l = remove_punctuation(sentence=l, punctuations=punctuations_to_remove)
                                     out.write(l + u',' + intent + u'\n')
                                 except UnicodeEncodeError as e:
                                     print "1.a Warning.", e.message,"Bad unicode character here: ", l
@@ -34,13 +72,15 @@ def dump_queries(inputfile = '', outquery = '', sheetnames = None, columns_with_
                         for l in df[col]:
                             if isinstance(l, unicode) or isinstance(l, str):
                                 try:
-                                    l = convert_dates_to_dummy_day(l)
-                                    l = remove_punctuation(sentence=l, punctuations=punctuations_to_remove)
+                                    if process_queries:
+                                        l = convert_dates_to_dummy_day(l)
+                                        l = remove_punctuation(sentence=l, punctuations=punctuations_to_remove)
                                     out.write(l + u',' + 'none' + u'\n')
                                 except UnicodeEncodeError as e:
                                     print "2.a Warning.", e.message,"Bad unicode character here: ", l
                                 except Exception as e:
                                     print "2.b Warning: Bad sentence: ", l, " ", e.message
+
 
 def remove_punctuation(sentence = '', punctuations = ''):
     for p in punctuations:
@@ -117,3 +157,8 @@ def plot_length_of_queries(input_file):
             lengths.append(len(l.split()))
     return lengths
 
+if __name__=='__main__':
+    dump_queries_from_excel(inputfile='data/intent_convs.xlsx',
+                            outputfile='data/intent_convs.csv',
+                            columns=['Query','Intent'],
+                            append=False, process_queries=False, punctuations_to_remove='.!?"~;,-/()')
